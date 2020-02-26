9f704569e199ef2015cab27e38350d11bca174ee
sieradz <sieradz628@gmail.com>
2020-02-26 09:04:07 +0100

    added pagination for articles in views

--- app/services/articleService.js
+++ app/services/articleService.js
@@ -1,9 +1,11 @@
 const prismic = require('../config/prismic').query;
 
-const getArticle = () => new Promise ((resolve) => {
+const getArticle = (page=1) => new Promise ((resolve) => {
   let query = [ // let's ask for articles sorted by date
     `q=[[at(document.type,"article")]]`,
-    `orderings=[document.first_publication_date desc]`
+    `orderings=[document.first_publication_date desc]`,
+    `pageSize=2`,
+    `page=${page}`,
   ];
   prismic(query).then(results => {
     resolve(results);
